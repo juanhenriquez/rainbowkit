57aa46b4d34cf3591d97b179926a386033126812
Juan Henriquez <hello@juanhz.dev>
2022-07-20 01:42:50 -0400

    export modal components

--- packages/rainbowkit/src/components/index.ts
+++ packages/rainbowkit/src/components/index.ts
@@ -1,2 +1,5 @@
 export { ConnectButton } from './ConnectButton/ConnectButton';
+export { ConnectModal } from './ConnectModal/ConnectModal';
+export { AccountModal } from './AccountModal/AccountModal';
+export { ChainModal } from './ChainModal/ChainModal';
 export { RainbowKitProvider } from './RainbowKitProvider/RainbowKitProvider';
