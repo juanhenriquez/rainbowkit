5d51b59984fbd361cf1fa7f1c964ebc55ac720f0
Juan Henriquez <hello@juanhz.dev>
2022-07-20 03:53:46 -0400

    chore: add container as params to the ConnectModal

--- packages/rainbowkit/src/components/ConnectModal/ConnectModal.tsx
+++ packages/rainbowkit/src/components/ConnectModal/ConnectModal.tsx
@@ -4,16 +4,22 @@ import ConnectOptions from '../ConnectOptions/ConnectOptions';
 import { Dialog } from '../Dialog/Dialog';
 import { DialogContent } from '../Dialog/DialogContent';
 export interface ConnectModalProps {
+  container?: HTMLElement;
   open: boolean;
   onClose: () => void;
 }
 
-export function ConnectModal({ onClose, open }: ConnectModalProps) {
+export function ConnectModal({ container, onClose, open }: ConnectModalProps) {
   const titleId = 'rk_connect_title';
   const { isConnected } = useAccount();
 
   return !isConnected ? (
-    <Dialog onClose={onClose} open={open} titleId={titleId}>
+    <Dialog
+      container={container}
+      onClose={onClose}
+      open={open}
+      titleId={titleId}
+    >
       <DialogContent bottomSheetOnMobile padding="0" wide>
         <ConnectOptions onClose={onClose} />
       </DialogContent>
