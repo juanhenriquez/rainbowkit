4344c59b9f3190d1486d9a6f727cbe569ddb7b72
Juan Henriquez <hello@juanhz.dev>
2022-07-20 03:29:53 -0400

    chore: export ConnectOptions

--- packages/rainbowkit/src/components/index.ts
+++ packages/rainbowkit/src/components/index.ts
@@ -3,3 +3,5 @@ export { ConnectModal } from './ConnectModal/ConnectModal';
 export { AccountModal } from './AccountModal/AccountModal';
 export { ChainModal } from './ChainModal/ChainModal';
 export { RainbowKitProvider } from './RainbowKitProvider/RainbowKitProvider';
+
+export { default as ConnectOptions } from './ConnectOptions/ConnectOptions';
